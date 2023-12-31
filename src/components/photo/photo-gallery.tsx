8f061d9f634c1e85e8b70458993757275195b770
Stuart Finley <finleysg@gmail.com>
2023-12-31 17:25:56 -0600

    Change the behavior of the photo gallery to load additional images on demand.

--- src/components/photo/photo-gallery.tsx
+++ src/components/photo/photo-gallery.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react"
+import { useState } from "react"
 
 import { usePagingData } from "../../hooks/use-paging-data"
 import { Photo, PhotoApiSchema } from "../../models/photo"
@@ -10,62 +10,49 @@ interface PhotoGalleryProps {
 }
 
 export function PhotoGallery({ tag }: PhotoGalleryProps) {
-  const [url, setUrl] = useState(() => {
-    if (tag) {
-      return `photos/?page=1&tags=${tag}`
-    } else {
-      return "photos/?page=1"
-    }
-  })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { results, next, loading } = usePagingData<any>(url, PhotoApiSchema)
-  const loader = useRef<IntersectionObserver>()
+  const [page, setPage] = useState(1)
+  const [photos, setPhotos] = useState<Photo[]>([])
 
-  const lastPhotoRef = useCallback(
-    (node: HTMLLIElement) => {
-      if (loading) return
-      if (loader.current) loader.current.disconnect()
-      loader.current = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting && next) {
-            setUrl(next)
-          }
-        },
-        {
-          root: null,
-          rootMargin: "20px",
-          threshold: 0,
-        },
-      )
-      if (node) {
-        loader.current.observe(node)
-      }
-    },
-    [loading, next],
-  )
+  const url = () => {
+    if (tag) {
+      return `photos/?page=${page}&tags=${tag}`
+    } else {
+      return `photos/?page=${page}`
+    }
+  }
+  const { data, status } = usePagingData(url(), PhotoApiSchema)
+
+  console.log("results length: " + data?.results?.length)
+  console.log("page: " + page)
+  console.log("photos length: " + photos.length)
+
+  if (data?.results && (data?.results?.length ?? 0) * page > photos.length) {
+    setPhotos([...photos, ...data.results.map((pic) => new Photo(pic))])
+  }
+
+  const handleLoadMore = () => {
+    setPage(page + 1)
+  }
 
   return (
-    <ul style={{ listStyle: "none" }}>
-      {results?.map((pic, index) => {
-        if (results.length === index + 1) {
-          return (
-            <li key={pic.id} ref={lastPhotoRef} style={{ display: "inline-block" }}>
-              <SmallPhoto photo={new Photo(pic)} />
-            </li>
-          )
-        } else {
+    <div>
+      <OverlaySpinner loading={status === "pending"} />
+      <ul style={{ listStyle: "none" }}>
+        {photos.map((pic) => {
           return (
             <li key={pic.id} style={{ display: "inline-block" }}>
-              <SmallPhoto photo={new Photo(pic)} />
+              <SmallPhoto photo={pic} />
             </li>
           )
-        }
-      })}
-      {next && (
-        <li>
-          <OverlaySpinner loading={loading} />
-        </li>
-      )}
-    </ul>
+        })}
+        {data?.next && (
+          <li>
+            <button className="btn btn-link" onClick={handleLoadMore}>
+              load more...
+            </button>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
--- src/hooks/use-paging-data.ts
+++ src/hooks/use-paging-data.ts
@@ -1,48 +1,32 @@
-import { useCallback, useEffect, useState } from "react"
-
 import { z, ZodType } from "zod"
+
+import { useQuery } from "@tanstack/react-query"
 
 import { getOne } from "../utils/api-client"
 
 function createPagingSchema<ItemType extends z.ZodTypeAny>(itemSchema: ItemType) {
   return z.object({
     count: z.number(),
-    next: z.string().url(),
-    previous: z.string().url(),
+    next: z.string().url().nullable(),
+    previous: z.string().url().nullable(),
     results: z.array(itemSchema),
   })
 }
 
-export function usePagingData<TData extends z.ZodTypeAny>(url: string, schema: ZodType) {
-  const [results, setResults] = useState<TData[]>([])
-  const [next, setNext] = useState("")
-  const [count, setCount] = useState(0)
-  const [loading, setLoading] = useState(false)
-
+export function usePagingData(url: string, schema: ZodType) {
   const pagingSchema = createPagingSchema(schema)
   type pagingData = z.infer<typeof pagingSchema>
 
-  const fetchPage = useCallback(async () => {
-    // The data returned from a paging url is
-    // count (number), next (url), previous (url),
-    // and results (array)
-    try {
-      setLoading(true)
-      const data = await getOne<pagingData>(url, pagingSchema)
-      if (data) {
-        setNext(data.next)
-        setCount(data.count)
-        setResults((prev) => [...prev, ...data.results])
+  return useQuery({
+    queryKey: [url],
+    queryFn: () => getOne<pagingData>(url, pagingSchema),
+    select: (data) => {
+      return {
+        count: data?.count,
+        next: data?.next,
+        previous: data?.previous,
+        results: data?.results.map((item) => schema.parse(item)) ?? [],
       }
-      setLoading(false)
-    } finally {
-      setLoading(false)
-    }
-  }, [pagingSchema, url])
-
-  useEffect(() => {
-    fetchPage()
-  }, [fetchPage])
-
-  return { next, count, results, loading }
+    },
+  })
 }
