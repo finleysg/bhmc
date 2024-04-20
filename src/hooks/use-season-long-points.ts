667ac575c9bcf1f65b83946968b8b36c618fb3cc
Stuart Finley <finleysg@gmail.com>
2024-04-20 16:04:43 -0500

    Separate season long points hook and model from top points.

--- src/hooks/use-season-long-points.ts
+++ src/hooks/use-season-long-points.ts
@@ -1,20 +1,52 @@
 import { useQuery } from "@tanstack/react-query"
 
-import { TopPoints, TopPointsData, TopPointsSchema } from "../models/points"
+import {
+  Points,
+  PointsApiSchema,
+  PointsData,
+  TopPoints,
+  TopPointsData,
+  TopPointsSchema,
+} from "../models/points"
 import { getMany } from "../utils/api-client"
 
-interface SeasonLongPointsArgs {
+interface TopPointsArgs {
   season: number
   topN: number
   category: "gross" | "net"
 }
 
-export function useSeasonLongPoints({ season, topN, category }: SeasonLongPointsArgs) {
+export function useTopPoints({ season, topN, category }: TopPointsArgs) {
   const endpoint = `points/${season}/${category}/${topN}/`
 
   return useQuery({
     queryKey: ["season-long-points", season, category, topN],
     queryFn: () => getMany<TopPointsData>(endpoint, TopPointsSchema),
     select: (data) => data.map((pt) => new TopPoints(pt)),
+  })
+}
+
+interface SeasonLongPointsArgs {
+  season?: number
+  eventId?: number
+  playerId?: number
+}
+
+export function useSeasonLongPoints({ season, eventId, playerId }: SeasonLongPointsArgs) {
+  let endpoint = "season-long-points/"
+  if (season) {
+    endpoint += `?season=${season}` + (playerId ? `&player=${playerId}` : "")
+  } else if (eventId) {
+    endpoint += `?event=${eventId}`
+  } else {
+    throw new Error(
+      "You must provide either a season or an eventId to retrieve season long points.",
+    )
+  }
+
+  return useQuery({
+    queryKey: ["season-long-points", season ?? 0, playerId ?? 0, eventId ?? 0],
+    queryFn: () => getMany<PointsData>(endpoint, PointsApiSchema),
+    select: (data) => data.map((pt) => new Points(pt)),
   })
 }
