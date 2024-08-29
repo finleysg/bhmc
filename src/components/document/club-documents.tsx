3cd6f287d75edaa46293115f902ee1f5456d8613
Stuart Finley <finleysg@gmail.com>
2024-08-29 11:42:17 -0500

    Date formatting adjustment for date-only fields.

--- src/components/document/club-documents.tsx
+++ src/components/document/club-documents.tsx
@@ -7,6 +7,8 @@ export function ClubDocumentsCard() {
         <h4 className="card-header">Club Documents</h4>
         <ClubDocument code="BYLAW" documentType="O" />
         <ClubDocument code="FIN" documentType="F" />
+        <ClubDocument code="ACCTS" documentType="O" />
+        <ClubDocument code="SO" documentType="O" />
         <ClubDocument code="HCP" documentType="O" />
         <ClubDocument code="TUT1" documentType="O" />
         <ClubDocument code="TUT2" documentType="O" />
