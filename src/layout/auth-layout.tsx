5aa179a8d08a6e6d56df7ccf1f805d17ff2ee2c6
Stuart Finley <finleysg@gmail.com>
2025-01-19 08:26:13 -0600

    Add session routes to maintenance mode. Support Apple Pay.

--- src/layout/auth-layout.tsx
+++ src/layout/auth-layout.tsx
@@ -4,15 +4,24 @@ import { ToastContainer } from "react-toastify"
 import { ActivateAccountScreen } from "../screens/activate-account"
 import { ConfirmAccountScreen } from "../screens/confirm-account"
 import { LoginScreen } from "../screens/login"
+import { MaintenanceScreen } from "../screens/maintenance"
 import { NotFoundScreen } from "../screens/not-found"
 import { RegisterAccountScreen } from "../screens/register-account"
 import { ResetPasswordScreen } from "../screens/reset-password"
 import { ResetPasswordCompleteScreen } from "../screens/reset-password-complete"
 import { ResetPasswordRequestScreen } from "../screens/reset-password-request"
 import { ResetPasswordSentScreen } from "../screens/reset-password-sent"
+import * as config from "../utils/app-config"
 import { AuthHeader } from "./auth-header"
 
 function SessionRoutes() {
+  if (config.maintenanceMode) {
+    return (
+      <Routes>
+        <Route path="*" element={<MaintenanceScreen />} />
+      </Routes>
+    )
+  }
   return (
     <Routes>
       <Route path="/login" element={<LoginScreen />} />
