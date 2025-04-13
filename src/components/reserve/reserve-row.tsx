9c830b784537328936dbf45c8fe2eabb952ea193
Stuart Finley <finleysg@gmail.com>
2025-04-12 20:26:43 -0500

    Add 4 signup waves for Wednesday event registration.

--- src/components/reserve/reserve-row.tsx
+++ src/components/reserve/reserve-row.tsx
@@ -7,6 +7,7 @@ interface ReserveRowProps extends Omit<ComponentPropsWithoutRef<"div">, "onSelec
   courseName: string
   group: ReserveGroup
   mode: "view" | "edit"
+  wave: number
   onSelect: (slot: ReserveSlot[]) => void
   onReserve: (groupName: string) => void
 }
@@ -15,26 +16,52 @@ export function ReserveRow({
   courseName,
   group,
   mode,
+  wave,
   onSelect,
   onReserve,
   ...rest
 }: ReserveRowProps) {
+  const waveAvailable = () => {
+    if (wave > 0) {
+      return wave >= group.wave
+    }
+    return true
+  }
+
+  const availabilityMessage = () => {
+    if (!waveAvailable()) {
+      let startTime = "5:00 PM"
+      if (group.wave === 2) {
+        startTime = "5:15 PM"
+      } else if (group.wave === 3) {
+        startTime = "5:30 PM"
+      } else if (group.wave === 4) {
+        startTime = "5:45 PM"
+      }
+      return `Available at ${startTime}`
+    }
+    return undefined
+  }
+
   return (
-    <div className={`reserve-group reserve-group__${courseName.toLowerCase()}`} {...rest}>
+    <div
+      className={`reserve-group reserve-group__${courseName.toLowerCase()} ${!waveAvailable() ? "reserve-group__unavailable" : ""}`}
+      {...rest}
+    >
       <div className={mode === "edit" ? "reserve-group-name" : "reserved-group-name"}>
         <span>{group.name}</span>
         {mode === "edit" && (
           <>
             <button
               className="btn btn-sm btn-info"
-              disabled={!group.hasOpenings()}
+              disabled={!group.hasOpenings() || !waveAvailable()}
               onClick={() => onSelect(group.slots)}
             >
               Select
             </button>
             <button
               className="btn btn-sm btn-warning"
-              disabled={group.isDisabled()}
+              disabled={group.isDisabled() || !waveAvailable()}
               onClick={() => onReserve(group.name)}
             >
               Register
@@ -43,7 +70,13 @@ export function ReserveRow({
         )}
       </div>
       {group.slots.map((slot) => (
-        <ReserveCard key={slot.id} reserveSlot={slot} onSelect={(slot) => onSelect([slot])} />
+        <ReserveCard
+          key={slot.id}
+          reserveSlot={slot}
+          onSelect={(slot) => onSelect([slot])}
+          isAvailable={waveAvailable()}
+          availabilityText={availabilityMessage()}
+        />
       ))}
     </div>
   )
